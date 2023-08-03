9e2afe5629257ff28ed6a7b0b185b091d3eb4b82
isaque sene <isaqueconcursos26@gmail.com>
2023-08-02 23:19:56 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+/* import { useState } from 'react' */
+
+function App() {
+  /* const [count, setCount] = useState(0) */
+
+  return (
+    <div className='app'>
+      App
+    </div>
+  )
+}
+
+export default App
