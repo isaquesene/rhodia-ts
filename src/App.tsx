6241e8f771df91be4fe06454981f3db817f9f670
isaque sene <isaqueconcursos26@gmail.com>
2023-08-03 21:47:59 -0300

    feat: page navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 /* import { useState } from 'react' */
+import Navbar from "@/scenes/navbar";
 
 function App() {
   /* const [count, setCount] = useState(0) */
 
   return (
-    <div className='app'>
-      Rhodia Page
+    <div className='app bg-gray-20'>
+      <Navbar />
     </div>
   )
 }
